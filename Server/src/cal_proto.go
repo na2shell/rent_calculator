fcbda7edc90b4c3538b8b58872ebcbe7d046e3c6
na2 <51122019+na2shell@users.noreply.github.com>
2021-09-25 15:51:50 +0900

    コマンドライン引数を取るようにした

--- Server/src/cal_proto.go
+++ Server/src/cal_proto.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	const rent int = 70000
-	const init_pay int = 300000
+	var (
+		rent     = flag.Int("r", 70000, "int flag")
+		init_pay = flag.Int("ini", 300000, "int flag")
+	)
+
+	flag.Parse()
 	const support int = 27000
 	const internet int = 2800
 
-	var res = rent + int(init_pay/24) + internet - support
+	var res = *rent + int(*init_pay/24) + internet - support
 	fmt.Printf("result:%d\n", res)
 }
